3d8d8a8535b2d2d9ba05372b84030ce3c864e80b
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-06-29 11:33:49 +0300

    add new Pizza constructor function in the javascript file.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 
-var smallPizza = new Pizza
+function Pizza(price, crust,  toppings, quantity){
+  this.price = price;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.quantity = quantity;
+}
+
+var smallPizza = new Pizza(500, ["Crispy", "Stuffed", "Gluten-free"], ["Pepperoni", "Mushroom", "Green Pepper", "Fresh Garlic", "Tomato", "Black Olives", "Sausage", "Onions", "Fresh Basil", "Extra Cheese"], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+var mediumPizza = new Pizza(800, ["Crispy", "Stuffed", "Gluten-free"], ["Pepperoni", "Mushroom", "Green Pepper", "Fresh Garlic", "Tomato", "Black Olives", "Sausage", "Onions", "Fresh Basil", "Extra Cheese"], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+var largePizza = new Pizza(1300, ["Crispy", "Stuffed", "Gluten-free"], ["Pepperoni", "Mushroom", "Green Pepper", "Fresh Garlic", "Tomato", "Black Olives", "Sausage", "Onions", "Fresh Basil", "Extra Cheese"], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
