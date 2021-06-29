5f0318671dbd702d9b92e3ef612b22aaf0b8891a
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-06-29 21:26:28 +0300

    add calculateTotal javascript function to compute total price of order.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 
-function Pizza(price, crust,  toppings, quantity){
+function Pizza(price, crust, toppings,){
   this.price = price;
   this.crust = crust;
   this.toppings = toppings;
-  this.quantity = quantity;
 }
 
-var smallPizza = new Pizza(500, 50, 100, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-var mediumPizza = new Pizza(800, 150, 250, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-var largePizza = new Pizza(1300, 250, 450, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+var smallPizza = new Pizza(500, 50, 100);
+var mediumPizza = new Pizza(800, 150, 250);
+var largePizza = new Pizza(1300, 250, 450);
 
 const deliveryFeeWithinCBD = 100;
 const deliveryFeeOutsideCBD = 200;
 
 Pizza.prototype.addTotalCost = function(){
   return this.price+this.crust+this.toppings;
+}
+
+
+function calculateTotal(){
+
+  var smallQuantity = parseInt(document.getElementById("s-quantity").value);
+  var mediumQuantity = parseInt(document.getElementById("m-quantity").value);
+  var largeQuantity = parseInt(document.getElementById("l-quantity").value);
+
+  totalPurchase = smallPizza.addTotalCost() * smallQuantity + mediumPizza.addTotalCost() *mediumQuantity + (largePizza.addTotalCost() * largeQuantity);
+
+  return totalPurchase;
 }
