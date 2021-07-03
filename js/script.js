6f6da855fba2fdb4e6b13342a3b18b440bf2ae8c
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-07-03 16:47:01 +0300

    add DRY code to html page to selecte pizza type, piza size, pizza crust, and checkbox to alow selection of more than one pizza topping.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+//Have an empty aray to store toppings selected
+//Check  pizza type selected
+//Have a variable total to store the total price
+//Check pizza size selected and add its price to total
+//Check pizza crust selected and add its price to total
+//Store the topppings selected in the array
+//Check the prices of the toppings selected and add it to total 
+//Get the quantity entered and multiply it by its total to get the total price
+//Return the total price
+
 
 function Pizza(price, crust, toppings,){
   this.price = price;
@@ -22,6 +32,11 @@ function calculateTotal(){
   var smallQuantity = parseInt(document.getElementById("s-quantity").value);
   var mediumQuantity = parseInt(document.getElementById("m-quantity").value);
   var largeQuantity = parseInt(document.getElementById("l-quantity").value);
+ 
+  console.log(smallQuantity);
+  console.log(mediumQuantity);
+  console.log(largeQuantity);
+
 
   totalPurchase = smallPizza.addTotalCost() * smallQuantity + mediumPizza.addTotalCost() *mediumQuantity + (largePizza.addTotalCost() * largeQuantity);
 
